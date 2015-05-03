001d8d43014a62b53d612f675ef83995bb286b65
sparklyballs <sparklyballs@gmail.com>
2015-05-03 19:37:21 +0100

    Delete 04-copy-script.sh

--- handbrake-scriptable/04-copy-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chown -R nobody:users /script
-if [ -f "/convert.sh" ]; then
-echo "using existing script, may require editing"
-chown -R nobody:users /script
-else
-cp /convert.sh /script/convert.sh
-chown -R nobody:users /script
-fi
