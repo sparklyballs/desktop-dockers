7779fa363022a907a0a4b18e36316c42b46177a4
sparklyballs <sparklyballs@gmail.com>
2015-05-05 01:22:14 +0100

    Update copy-script.sh

--- handbrake-scriptable/src/copy-script.sh
+++ handbrake-scriptable/src/copy-script.sh
@@ -3,7 +3,9 @@ chown -R nobody:users /script
 if [ -f "/script/convert.sh" ]; then
 echo "using existing script, may require editing"
 chown -R nobody:users /script
+chmod +x /script/convert.sh
 else
 cp /convert.sh /script/convert.sh
+chmod +x /script/convert.sh
 chown -R nobody:users /script
 fi
