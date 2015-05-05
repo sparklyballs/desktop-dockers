21d9895dbccaa2c25e1c82ed1475205dd20406f9
sparklyballs <sparklyballs@gmail.com>
2015-05-05 02:16:40 +0100

    Update convert.sh

--- handbrake-scriptable/src/convert.sh
+++ handbrake-scriptable/src/convert.sh
@@ -1,4 +1,31 @@
-
 #!/bin/bash
 
-echo "some stuff in here"
+########### EDITABLE VARIABLES ##########
+
+DEST_EXT=mp4
+PRESET=Ipad
+
+#########################################
+
+
+
+##### DO NOT CHANGE ANYTHING BELOW HERE ###############
+
+SRC=/watch-folder
+DEST=/Output
+HANDBRAKE_CLI=HandBrakeCLI
+
+
+
+IFS=$(echo -en "\n\b")
+for FILE in `ls $SRC`
+do
+        filename=$(basename $FILE)
+        extension=${filename##*.}
+        filename=${filename%.*}
+
+        $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset $PRESET
+
+chown nobody:users $DEST/$filename.$DEST_EXT
+
+done
