89a5eefd8fe8be7757cf8474a13e5bb19881672e
sparklyballs <sparklyballs@gmail.com>
2015-05-06 19:04:43 +0100

    Update inotify.sh

--- handbrake/src/inotify.sh
+++ handbrake/src/inotify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while inotifywait -r -e create /watch-folder; do
+while inotifywait -r -e create /nobody/.config/ghb/Watch-Folder; do
 /bin/bash /nobody/.config/ghb/Convert-Script/convert.sh
 echo "Converting file(s)"
 done
