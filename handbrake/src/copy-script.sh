c9bd55f8fca68858953cda65ffee5a91f21b0dfe
sparklyballs <sparklyballs@gmail.com>
2015-05-06 19:02:27 +0100

    Update copy-script.sh

--- handbrake/src/copy-script.sh
+++ handbrake/src/copy-script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-chown -R nobody:users /script
-if [ -f "/script/convert.sh" ]; then
+mkdir -p /nobody/.config/ghb/Convert-Script
+chown -R nobody:users /nobody/.config/ghb
+if [ -f "/nobody/.config/ghb/Convert-Script/convert.sh" ]; then
 echo "using existing script, may require editing"
-chown -R nobody:users /script
-chmod +x /script/convert.sh
+chown -R nobody:users /nobody/.config/ghb
+chmod +x /nobody/.config/ghb/Convert-Script/convert.sh
 else
-cp /convert.sh /script/convert.sh
-chmod +x /script/convert.sh
-chown -R nobody:users /script
+cp /convert.sh /nobody/.config/ghb/Convert-Script/convert.sh
+chmod +x /nobody/.config/ghb/Convert-Script/convert.sh
+chown -R nobody:users /nobody/.config/ghb
 fi
