584b3454a9f379192fa70cd70c76f3645ba10f3b
David Coppit <david@coppit.org>
2017-04-04 22:29:09 -0400

    Run the process in the background to renice it

--- handbrake/src/convert.sh
+++ handbrake/src/convert.sh
@@ -24,7 +24,7 @@ do
         extension=${filename##*.}
         filename=${filename%.*}
 
-        $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset $PRESET
+        $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset $PRESET &
 
         PID=$!
         # This might fail if the user has not provided --cap-add=CAP_SYS_NICE to the container. So ignore failures here.
